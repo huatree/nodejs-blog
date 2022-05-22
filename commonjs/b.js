85a280d21d03c8fc1c1561ee0dbd5ef201b3e9dd
huatree <wang.yaohua@foxmail.com>
2022-05-22 13:12:16 +0800

    ✨ feat(commonjs): 使用lodash依赖

--- commonjs/b.js
+++ commonjs/b.js
@@ -1,4 +1,9 @@
 const opts = require('./a')
+const _ = require('lodash')
+
 const sum = opts.add(10, 20)
 const result = opts.mul(10, 20)
 console.log(sum, result)
+
+const arr = _.concat([1, 2], 3)
+console.log(arr)
