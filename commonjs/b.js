771c5b07be05755eed9303560ac5eaa690032086
huatree <wang.yaohua@foxmail.com>
2022-05-22 12:57:10 +0800

    ✨ feat(commonjs): 更新输出多个函数的整体写法

--- commonjs/b.js
+++ commonjs/b.js
@@ -1,4 +1,4 @@
-const { add, mul } = require('./a')
-const sum = add(10, 20)
-const result = mul(10, 20)
+const opts = require('./a')
+const sum = opts.add(10, 20)
+const result = opts.mul(10, 20)
 console.log(sum, result)
