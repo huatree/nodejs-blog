33c0e3edf49a26b57808fdbac8cd22004c74ef4f
huatree <wang.yaohua@foxmail.com>
2022-05-22 23:02:53 +0800

    ✨ feat(blog-native): 封装获取POST请求体

--- blog-native/app.js
+++ blog-native/app.js
@@ -2,27 +2,56 @@ const url = require('url')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+const getPostData = (req) => {
+  const promise = new Promise((resolve, reject) => {
+    if (req.method !== 'POST') {
+      resolve({})
+      return
+    }
+    if (req.headers['content-type'] !== 'application/json') {
+      resolve({})
+      return
+    }
+    let postData = ''
+    req.on('data', (chunk) => {
+      postData += chunk.toString()
+    })
+    req.on('end', () => {
+      if (!postData) {
+        resolve({})
+        return
+      }
+      resolve(JSON.parse(postData))
+    })
+  })
+  return promise
+}
+
 const serverHandle = (req, res) => {
   res.setHeader('content-type', 'application/json')
   req.path = req.url.split('?')[0]
   req.query = url.parse(req.url, true).query
 
-  const blogData = handleBlogRouter(req, res)
-  if(blogData) {
-    res.end(JSON.stringify(blogData))
-    return
-  }
+  getPostData(req).then((postData) => {
+    req.body = postData
 
-  const userData = handleUserRouter(req, res)
-  if(userData) {
-    res.end(JSON.stringify(userData))
-    return
-  }
+    const blogData = handleBlogRouter(req, res)
+    if (blogData) {
+      res.end(JSON.stringify(blogData))
+      return
+    }
 
-  // 未命中路由，返回404
-  res.writeHead(404, {'content-type': 'text/plain'})
-  res.write('404 Not Found\n')
-  res.end()
+    const userData = handleUserRouter(req, res)
+    if (userData) {
+      res.end(JSON.stringify(userData))
+      return
+    }
+
+    // 未命中路由，返回404
+    res.writeHead(404, { 'content-type': 'text/plain' })
+    res.write('404 Not Found\n')
+    res.end()
+  })
 }
 
 module.exports = serverHandle
