9fe56c018c239e01a3b798bcce3ab8be4d80c626
huatree <wang.yaohua@foxmail.com>
2022-05-22 22:24:41 +0800

    ✨ feat(blog-native): 实现博客详情路由

--- blog-native/src/controller/blog.js
+++ blog-native/src/controller/blog.js
@@ -18,6 +18,18 @@ const getList = (author, keyword) => {
   ]
 }
 
+const getDetail = (id) => {
+  // mock
+  return {
+    id: 1,
+    title: '标题A',
+    content: '内容A',
+    createTime: '1653219335012',
+    author: '张三'
+  }
+}
+
 module.exports = {
-  getList
+  getList,
+  getDetail
 }
