fe9561a1a0409c4ffcb7b98cfe69555a4e0555ec
huatree <wang.yaohua@foxmail.com>
2022-05-24 13:16:25 +0800

    ✨ feat(blog-native): 实现cookie过期限制

--- blog-native/src/router/user.js
+++ blog-native/src/router/user.js
@@ -1,5 +1,12 @@
 const { signIn } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000)
+  return d.toUTCString()
+}
+
 const handleUserRouter = (req, res) => {
   const { method, path, query, cookie } = req
 
@@ -8,7 +15,7 @@ const handleUserRouter = (req, res) => {
     const { username, password } = query
     return signIn(username, password).then((result) => {
       if (result.username) {
-        res.setHeader('Set-Cookie', `username=${result.username}; path=/; httpOnly`)
+        res.setHeader('Set-Cookie', `username=${result.username}; path=/; httpOnly; expires=${getCookieExpires()}`)
         return new SuccessModel()
       }
       return new ErrorModel('登录失败')
@@ -18,9 +25,11 @@ const handleUserRouter = (req, res) => {
   // 登录验证测试
   if (method === 'GET' && req.path === '/api/user/login-test') {
     if (cookie.username) {
-      return Promise.resolve(new SuccessModel({
-        username: cookie.username
-      }))
+      return Promise.resolve(
+        new SuccessModel({
+          username: cookie.username
+        })
+      )
     }
     return Promise.resolve(new ErrorModel('尚未登录'))
   }
