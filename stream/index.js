248f1f8e779c13d2aa445383538e879ebb1a6c07
huatree <wang.yaohua@foxmail.com>
2022-05-28 23:04:43 +0800

    ✨ feat(stream): 实现流的管道应用

--- stream/index.js
+++ stream/index.js
@@ -1,2 +1,8 @@
-// 标准输入输出
-process.stdin.pipe(process.stdout)
+const http = require('http')
+ http
+ .createServer((req, res) => {
+  if(req.method === 'POST') {
+    req.pipe(res)
+  }
+})
+.listen(8000)
