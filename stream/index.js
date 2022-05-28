674460b779a712da44a6f049e70b5f9d1c35451d
huatree <wang.yaohua@foxmail.com>
2022-05-28 23:28:30 +0800

    ✨ feat(stream): 实现HTTP请求文件读取

--- stream/index.js
+++ stream/index.js
@@ -1,18 +1,13 @@
 const fs = require('fs')
 const path = require('path')
+const http = require('http')
 
 const fileName1 = path.resolve(__dirname, 'data.txt')
-const fileName2 = path.resolve(__dirname, 'data-bak.txt')
-
-const readStream = fs.createReadStream(fileName1)
-const writeStream = fs.createWriteStream(fileName2)
-
-readStream.pipe(writeStream)
-// 监听流动的数据
-readStream.on('data', (chunk) => {
-  console.log(chunk.toString())
-})
-// 数据流动结束后的打印
-readStream.on('end', () => {
-  console.log('copy done')
-})
+http
+  .createServer((req, res) => {
+    if (req.method === 'GET') {
+      const readStream = fs.createReadStream(fileName1)
+      readStream.pipe(res)
+    }
+  })
+  .listen(8000)
