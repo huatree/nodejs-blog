1961372663742608cfa4d43c770d04658f25ea3f
huatree <wang.yaohua@foxmail.com>
2022-05-28 23:22:46 +0800

    ✨ feat(stream): 实现文件备份

--- stream/index.js
+++ stream/index.js
@@ -1,8 +1,18 @@
-const http = require('http')
- http
- .createServer((req, res) => {
-  if(req.method === 'POST') {
-    req.pipe(res)
-  }
+const fs = require('fs')
+const path = require('path')
+
+const fileName1 = path.resolve(__dirname, 'data.txt')
+const fileName2 = path.resolve(__dirname, 'data-bak.txt')
+
+const readStream = fs.createReadStream(fileName1)
+const writeStream = fs.createWriteStream(fileName2)
+
+readStream.pipe(writeStream)
+// 监听流动的数据
+readStream.on('data', (chunk) => {
+  console.log(chunk.toString())
 })
-.listen(8000)
+// 数据流动结束后的打印
+readStream.on('end', () => {
+  console.log('copy done')
+})
