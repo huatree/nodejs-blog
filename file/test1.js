bfedb089cf7ed17c02bd906e31c653424be9b9e9
huatree <wang.yaohua@foxmail.com>
2022-05-28 21:43:08 +0800

    ✨ feat(file): 实现判断文件是否存在

--- file/test1.js
+++ file/test1.js
@@ -3,12 +3,4 @@ const path = require('path')
 
 const fileName = path.resolve(__dirname, './data.txt')
 
-const content = '这是写入的内容\n'
-const opt = {
-  flag: 'a' // 追加写入。覆盖用 'w'
-}
-
-fs.writeFile(fileName, content, opt, (err) => {
-  if (err) throw err
-  console.log('写入成功')
-})
+console.log(fs.existsSync(fileName))
