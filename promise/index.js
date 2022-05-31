3c83be522a39375066815597a6c92507c1cd413f
huatree <wang.yaohua@foxmail.com>
2022-05-31 11:14:05 +0800

    ✨ feat(promise): 实现async await写法

--- promise/index.js
+++ promise/index.js
@@ -15,14 +15,17 @@ function getFileContent(fileName) {
   return promise
 }
 
-async function readAData() {
-  const aData = await getFileContent('a.json')
-  return aData
+async function readFileData() {
+  try {
+    const aData = await getFileContent('a.json')
+    console.log('a data', aData)
+    const bData = await getFileContent(aData.next)
+    console.log('b data', bData)
+    const cData = await getFileContent(bData.next)
+    console.log('c data', cData)
+  } catch (error) {
+    console.error(error) // try catch截获Promise中的reject
+  }
 }
 
-async function test() {
-  const aData = await readAData()
-  console.log(aData)
-}
-
-test()
+readFileData()
