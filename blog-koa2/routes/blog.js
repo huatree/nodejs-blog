97fc1c029f0b7b1b48356b7f07dc105e5d2c4517
huatree <wang.yaohua@foxmail.com>
2022-05-31 20:50:58 +0800

    ✨ feat(blog-koa2): 实现博客和用户路由

--- blog-koa2/routes/blog.js
+++ blog-koa2/routes/blog.js
@@ -1,14 +1,50 @@
 const router = require('koa-router')()
 
+const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+const signInValid = require('../middleware/signInValid')
+
 router.prefix('/api/blog')
 
 router.get('/list', async (ctx, next) => {
-  const query = ctx.query
-  ctx.body = {
-    errno: 0,
-    query,
-    data: ['获取博客列表']
+  const { query, session } = ctx
+  if (query.isadmin === '1') {
+    if (!session.username) {
+      return (ctx.body = new ErrorModel('尚未登录'))
+    }
+    query.author = session.username
   }
+  const result = await getList(query.author || '', query.keyword || '')
+  ctx.body = new SuccessModel(result)
+})
+
+router.get('/detail', async (ctx, next) => {
+  const result = await getDetail(ctx.query.id)
+  ctx.body = new SuccessModel(result)
+})
+
+router.post('/new', signInValid, async (ctx, next) => {
+  const { request, session } = ctx
+  request.body.author = session.username
+  const result = await newBlog(request.body)
+  ctx.body = new SuccessModel(result)
+})
+
+router.post('/update', signInValid, async (ctx, next) => {
+  const { query, request } = ctx
+  const result = await updateBlog(query.id, request.body)
+  if (result) {
+    return (ctx.body = new SuccessModel())
+  }
+  ctx.body = new ErrorModel('更新博客失败')
+})
+
+router.post('/del', signInValid, async (ctx, next) => {
+  const result = await delBlog(ctx.query.id, ctx.session.username)
+  if (result) {
+    return (ctx.body = new SuccessModel())
+  }
+  ctx.body = new ErrorModel('删除博客失败')
 })
 
 module.exports = router
--- blog-koa2/routes/user.js
+++ blog-koa2/routes/user.js
@@ -1,24 +1,27 @@
 const router = require('koa-router')()
 
+const { signIn } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
 router.prefix('/api/user')
 
 router.post('/login', async (ctx, next) => {
   const { username, password } = ctx.request.body
-
-  ctx.body = {
-    errno: 0,
-    username,
-    password
+  const result = await signIn(username, password)
+  if (result.username) {
+    ctx.session.username = result.username
+    ctx.session.realname = result.realname
+    return (ctx.body = new SuccessModel(result))
   }
+  ctx.body = new ErrorModel('登录失败')
 })
 
-router.get('/session-test', async (ctx, next) => {
-  ctx.session.count = ctx.session.count || 0
-  ctx.session.count++
-  ctx.body = {
-    errno: 0,
-    count: ctx.session.count
+router.get('/signOut', async (ctx, next) => {
+  if (ctx.session) {
+    ctx.session = null
+    return (ctx.body = new SuccessModel('退出成功'))
   }
+  ctx.body = new SuccessModel('系统异常')
 })
 
 module.exports = router
